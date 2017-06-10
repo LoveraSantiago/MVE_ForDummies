6697831063f901d856a43c9a013a1f75ea1190b6
Santiago Lovera <santiago.lovera@gmail.com>
2017-06-09 23:18:32 -0300

    Adicionado possibilidade de listar todas as abreviações

--- src/principal/MVE_ForDummies.java
+++ src/principal/MVE_ForDummies.java
@@ -1,15 +1,31 @@
 package principal;
 
+import static conteudo.Abreviacoes.myValueOf;
+import static core.Avaliador.isPrintarTudo;
+import static core.Avaliador.isSaida;
+import static java.util.Arrays.asList;
+
+import conteudo.Abreviacoes;
 import core.Leitor;
 
 public class MVE_ForDummies {
 	
 	public static void main(String[] args) {		
 		try(Leitor leitor = new Leitor()){
-			while(true)
-				leitor.lerDoTeclado("Digite a abreviação.");
-		} catch (Exception e) {			
-			e.printStackTrace();
-		}	
+			while(true){				
+				String comando = leitor.lerDoTeclado("Digite a abreviação, listar ou sair");
+				if(isSaida(comando)) System.exit(0);
+				if(isPrintarTudo(comando))asList(Abreviacoes.values()).forEach(System.out::println);
+				else{					
+					try{			
+						System.out.println(myValueOf(comando));
+					}catch(IllegalArgumentException e){
+						System.out.println("Abreviação não encontrada. Maiusculas e minusculas são diferenciados.");
+					}
+				}
+				System.out.println();
+			}
+		}catch (Exception e) { e.printStackTrace(); }	
+		finally{System.out.println("Tchau tchau");}
 	}
 }
